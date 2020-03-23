a614cd0190390319f0a1aa4cd834848711b41e42
Lisa Jung <lisahjung@gmail.com>
2020-03-23 15:48:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,57 @@
+def using_push (array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(-2)
+end
+
+def using_shift(array)
+  array.shift()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=begin
 def using_push(array, string)
   array.push(string)
 end
@@ -45,3 +99,4 @@ end
 def using_delete_at(array, integer)
   array.delete_at(integer)
 end
+=end
