d79fc9275a6d85fc5c0fbcf8f85d9c37767090f4
Lisa Jung <lisahjung@gmail.com>
2020-03-23 15:46:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,32 +1,32 @@
-#def using_push(array, string)
+def using_push(array, string)
   array.push(string)
 end
 
-#def using_unshift(array, string)
+def using_unshift(array, string)
   array.unshift(string)
 end
 
-#def using_pop(array)
+def using_pop(array)
   array.pop()
 end
 
-#def pop_with_args(array)
+def pop_with_args(array)
   array.pop(2)
 end
 
-#def using_shift(array)
+def using_shift(array)
   array.shift()
 end
 
-#def shift_with_args(array)
+def shift_with_args(array)
   array.shift(2)
 end
 
-#def using_concat(array1,array2)
+def using_concat(array1,array2)
   array1.concat(array2)
 end
 
-#def using_insert(array, element)
+def using_insert(array, element)
   array.insert(4,element)
 end
 
@@ -38,10 +38,10 @@ def using_flatten(array)
   array.flatten 
 end
 
-#def using_delete(array, string)
+def using_delete(array, string)
   array.delete(array=string)
 end
 
-#def using_delete_at(array, integer)
+def using_delete_at(array, integer)
   array.delete_at(integer)
 end
