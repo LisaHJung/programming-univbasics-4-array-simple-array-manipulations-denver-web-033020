0d7c697c6ef39ef94413b68e4254e71374b30fdb
Lisa Jung <lisahjung@gmail.com>
2020-03-23 15:52:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -16,6 +16,35 @@ end
 
 def using_shift(array)
   array.shift()
+end
+
+def shfit_with_args(array)
+  array.shift(-2)
+end
+
+def using_concat(array1, array2)
+  array.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete
+end
+
+def using_delete_at(array, integer)
+  array.delete_at(index=integer)
+end
 
 
 
